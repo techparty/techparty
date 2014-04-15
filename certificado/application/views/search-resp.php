fe7f482cf447fda07eecdf76b27dffbdb568c4bf
Fernando Porazzi <fporazzi46@gmail.com>
2014-04-15 12:11:54 -0300

    Add number of days in the label

--- certificado/application/views/search-resp.php
+++ certificado/application/views/search-resp.php
@@ -4,7 +4,7 @@
   <?php foreach ($names as $n): ?>
     <li class="resp-list-item">
       <input type="checkbox" id="user-<?php echo $n->id; ?>" data-days="<?php echo $n->times; ?>">
-      <label for="user-<?php echo $n->id; ?>"><?php echo $n->name; ?></label>
+      <label for="user-<?php echo $n->id; ?>"><?php echo $n->name; ?> - <span class="bold">Número de dias: <?php echo $n->times; ?></span></label>
     </li>
   <?php endforeach; ?>
 </ul>
