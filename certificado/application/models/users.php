7b961c040258466db6156bc5bf7d4f06180049c7
Fernando Porazzi <fporazzi46@gmail.com>
2014-04-15 12:08:15 -0300

    Add group_by() and count() in the SQL

--- certificado/application/models/users.php
+++ certificado/application/models/users.php
@@ -8,8 +8,10 @@ class Users extends CI_Model {
 
   public function getNames ($val) {
 
-
+    $this->db->select("name, id, count(name) as times");
     $this->db->like('name', $val); // like na coluna `name`
+    $this->db->group_by('name');
+
 
     $names = $this->db->get('names'); // escolhe a tabela `names`
 
--- certificado/application/views/search-resp.php
+++ certificado/application/views/search-resp.php
@@ -1,8 +1,10 @@
+<?php //print_r($names); ?>
+
 <ul>
   <?php foreach ($names as $n): ?>
     <li class="resp-list-item">
-      <input type="checkbox" id="user-3" data-days="5">
-      <label for="user-3"><?php echo $n->name; ?></label>
+      <input type="checkbox" id="user-<?php echo $n->id; ?>" data-days="<?php echo $n->times; ?>">
+      <label for="user-<?php echo $n->id; ?>"><?php echo $n->name; ?></label>
     </li>
   <?php endforeach; ?>
 </ul>
