9e9cef4903a686b0bc154f20b3e7e30088591bd4
Fernando Porazzi <fporazzi46@gmail.com>
2014-04-15 12:15:57 -0300

    Add comments in the controller

--- certificado/application/controllers/search.php
+++ certificado/application/controllers/search.php
@@ -1,18 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Search extends CI_Controller {
+
   public function index() {
+    
+    // carrega a model de usuários
     $this->load->model('users');
 
+    // recebe nome passado na requisição ajax
     $username = $this->input->post('username');
 
+    // model `users` tem o método getNames() que retorna um array de acordo com o LIKE
     $data['names'] = $this->users->getNames($username);
-    //$data['names'] = array('name' => 'fernando');
 
-    #$data['username'] = $username;
-    #$data['status'] = 'ok';
-    #$data['message'] = ' server status';
-
+    // passa dados pra view
     $this->load->view('search-resp', $data);
   }
+
 }
--- certificado/application/models/users.php
+++ certificado/application/models/users.php
@@ -12,7 +12,6 @@ class Users extends CI_Model {
     $this->db->like('name', $val); // like na coluna `name`
     $this->db->group_by('name');
 
-
     $names = $this->db->get('names'); // escolhe a tabela `names`
 
     return $names->result();
