eb725f3fd4c6f210d3d67426fe60138d9aadd5b1
Daniel Röhers Moura <rohersmoura@gmail.com>
2014-12-01 10:04:23 -0200

    get twitter fail

--- 2015/assets/js/scripts.js
+++ 2015/assets/js/scripts.js
@@ -1,14 +1,19 @@
 
 $(document).ready(function(){/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
-})
-/* smooth scrolling for scroll down */
-$('.scroll-down').click(function(){
-  $('body,html').animate({scrollTop:$(window).scrollTop()+800},1000);
-})
 
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#navbar' })
+  $('.scroll-top').click(function(){
+    $('body,html').animate({scrollTop:0},1000);
+  })
+  /* smooth scrolling for scroll down */
+  $('.scroll-down').click(function(){
+    $('body,html').animate({scrollTop:$(window).scrollTop()+800},1000);
+  })
 
+  /* highlight the top nav as scrolling occurs */
+  $('body').scrollspy({ target: '#navbar' });
+
+  $.get('https://twitter.com/hashtag/techpartyfaccat')
+    .fail(function () {
+      document.querySelector('.twitter-list').style.display = 'none';
+    });
 });
