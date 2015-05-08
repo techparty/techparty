a553efc4168d6dc42a875d01209f25f1e5bcc90d
Fernando Porazzi <fporazzi46@gmail.com>
2015-05-08 16:00:23 -0300

    Add underscore template

--- 2016/assets/js/app.js
+++ 2016/assets/js/app.js
@@ -1,9 +1,45 @@
 /*! Author: github.com/fernandoporazzi */
 
+
 ;(function(window, document, undefined){
 
   'use strict';
 
+  var Speakers = (function() {
 
+    var _init = function () {
+      var xhr = new XMLHttpRequest();
+
+      xhr.open('GET', 'assets/js/data/speakers.json', true);
+      xhr.responseType = 'text';
+
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText),
+            speakersHolder = document.getElementById('speakers-holder'),
+            scriptTemplate = document.getElementById('speakers-template').innerHTML,
+            template;
+
+
+          _.templateSettings.variable = 'data';
+
+          template = _.template(scriptTemplate);
+
+          speakersHolder.innerHTML = template(data);
+
+          console.log(data);
+        }
+      };
+
+      xhr.send();
+    };
+
+    return {
+      init: _init
+    }
+
+  })();
+
+  Speakers.init();
 
 })(window, document);
