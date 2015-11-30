dccedeebd8f94685e6a6bf76149be0040960c203
fernandoporazzi <fernandoporazzi@gmail.com>
2015-11-30 15:22:20 -0200

    Add constructor to class

--- 2016/certificados/assets/js/Application.js
+++ 2016/certificados/assets/js/Application.js
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Application extends React.Component {
+
+  // React's getInitialState() should live inside the constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      obj: 'teste'
+    };
+  }
+
+  _fetch() {
+    console.log('fetch server data and then populate the state');
+  }
+
+  componentDidMount() {
+    this._fetch();
+  }
+
   render() {
-    return <div>Hello World</div>
+    return <div>{this.state.obj}</div>
   }
 }
 
