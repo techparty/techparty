60cce16580b291fabce6036529166e43dfd55585
fernandoporazzi <fernandoporazzi@gmail.com>
2015-12-01 21:30:17 -0200

    Add Loader

--- 2016/certificados/assets/js/Form.js
+++ 2016/certificados/assets/js/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCard from './UserCard';
+import Loader from './Loader';
 
 export default class Form extends React.Component {
   constructor(props) {
@@ -7,24 +8,51 @@ export default class Form extends React.Component {
 
     this.state = {
       showError: true,
-      showSuccess: false
+      showSuccess: true,
+      isFetchingData: false,
+
+      userEmail: '',
+      userCpf: ''
     };
+  }
+
+  _handleEmailChange(e) {
+    this.setState({userEmail: e.target.value});
+  }
+
+  _handleCpfChange(e) {
+    this.setState({userCpf: e.target.value});
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({isFetchingData: true});
+
+    console.log(this.state.userEmail, this.state.userCpf);
+
+    // Just to to check if states are working properly =)
+    // Remove later
+    setTimeout(() => {
+      this.setState({isFetchingData: false});
+    }, 4000);
   }
 
   render() {
     return (
       <div>
         <section className="card">
-          <form>
+          <form onSubmit={this._handleSubmit.bind(this)}>
             <div className="form-item">
-              <input type="email" name="email" id="email" required placeholder="E-mail" />
+              <input type="email" name="email" id="email" required placeholder="E-mail" onChange={this._handleEmailChange.bind(this)} />
             </div>
 
             <div className="form-item">
-              <input type="number" name="cpf" id="cpf" required placeholder="CPF" />
+              <input type="number" name="cpf" id="cpf" required placeholder="CPF" onChange={this._handleCpfChange.bind(this)} />
             </div>
 
             <div className="form-item">
+              {this.state.isFetchingData ? <Loader /> : false}
               <button className="btn-default">PESQUISAR</button>
             </div>
           </form>
