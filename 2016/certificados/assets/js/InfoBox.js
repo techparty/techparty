2e4896182d6ffc61ccb6c69c5b1360e1728f12e2
fernandoporazzi <fernandoporazzi@gmail.com>
2015-12-01 23:14:00 -0200

    Add logo box

--- 2016/certificados/assets/js/InfoBox.js
+++ 2016/certificados/assets/js/InfoBox.js
@@ -16,6 +16,10 @@ export default class InfoBox extends React.Component {
             <li><a href="mailto:rohersmoura@gmail.com">rohersmoura@gmail.com</a></li>
           </ul>
         </section>
+
+        <section className="logo-box">
+          <img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpf1/v/t34.0-12/11944454_909590355770757_677381732_n.jpg?oh=4aa418507c3c641eb9da69837a5a189a&oe=5661749E&__gda__=1449228483_462db22aea1588ffe0abdb44adb9c52d" />
+        </section>
       </div>
     );
   }
