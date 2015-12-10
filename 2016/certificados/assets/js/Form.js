780d7508bc84b647a04f107b94fac6ddf35aecbc
fernandoporazzi <fernandoporazzi@gmail.com>
2015-12-10 13:49:50 -0200

    Implement CPF validation

--- 2016/certificados/assets/js/Form.js
+++ 2016/certificados/assets/js/Form.js
@@ -25,6 +25,31 @@ export default class Form extends React.Component {
     this.setState({userCpf: e.target.value});
   }
 
+  _validateCpf(strCPF) {
+    // Código retirado do site da Receita Federal
+
+    var Soma;
+    var Resto;
+    Soma = 0;
+    var i;
+
+    if (strCPF == "00000000000") return false;
+
+    for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    return true;
+  }
+
   _handleSubmit(e) {
     e.preventDefault();
 
@@ -35,6 +60,12 @@ export default class Form extends React.Component {
     if (!email || !cpf) {
       this.setState({showError: true});
       this.setState({errorMessage: 'Preencha os campos E-mail e CPF'});
+      return;
+    }
+
+    if (!this._validateCpf(cpf)) {
+      this.setState({showError: true});
+      this.setState({errorMessage: 'CPF inválido'});
       return;
     }
 
