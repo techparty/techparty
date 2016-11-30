8f92621669e0430cfb3e2bd0106ac9000f061ffd
fernandoporazzi <fernandoporazzi@gmail.com>
2016-11-30 17:22:05 -0200

    Add google maps

--- 2017/assets/js/app.js
+++ 2017/assets/js/app.js
@@ -62,8 +62,30 @@
     }
   }
 
+  function initMap() {
+    var center = new google.maps.LatLng(-29.6389219,-50.7889245);
+
+    var properties = {
+      center: center,
+      zoom: 16,
+      scrollwheel: false,
+      draggable: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), properties);
+
+    var marker = new google.maps.Marker({
+      position: center,
+      icon: '/dist/img/techparty_icon.png'
+    });
+
+    marker.setMap(map);
+  }
+
   function init() {
     bindEvents();
+    initMap();
   }
 
   init();
